1d1726f20ba90872e4271eb234dbad76ec9449c7
Kelvin Rugut <krugut36@gmail.com>
2023-11-19 14:48:46 +0300

    Minor Tweak

--- pop.c
+++ pop.c
@@ -8,7 +8,8 @@
  */
 void pop_func(stack_t **head, unsigned int line_num)
 {
-	stack_t *top
+	stack_t *top;
+
 	if (*head == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_num);
